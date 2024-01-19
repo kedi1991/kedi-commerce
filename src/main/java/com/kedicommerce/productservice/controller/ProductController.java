11c8cbb395022c920a71872ceb24b677999edef7
OKURUT Henry Kedi <hokurut@netcompany.com>
2024-01-19 23:22:26 +0100

    add the create product service method from the controller class

--- src/main/java/com/kedicommerce/productservice/controller/ProductController.java
+++ src/main/java/com/kedicommerce/productservice/controller/ProductController.java
@@ -1,16 +1,24 @@
 package com.kedicommerce.productservice.controller;
 
 import com.kedicommerce.productservice.dto.ProductRequest;
+import com.kedicommerce.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService productService;
+    /**
+     * USE th final keyword to make the value assigned at constructor level only
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
-//productRequest is a DTO - data transfer object
+        //productRequest is a DTO - data transfer object
+        productService.createProduct(productRequest);
     }
 }
