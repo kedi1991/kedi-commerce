1795dc792e5d83b9c8e2ad80bea413df73a48b6f
OKURUT Henry Kedi <hokurut@netcompany.com>
2024-01-21 00:08:03 +0100

    setup testing class to load dynamic properties

--- src/test/java/com/kedicommerce/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/kedicommerce/productservice/ProductServiceApplicationTests.java
@@ -2,9 +2,26 @@ package com.kedicommerce.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestConstructor;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class ProductServiceApplicationTests {
+	/**
+	 * the mongoDBContainer no-args constructor is deprecated
+	 */
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+@DynamicPropertySource
+static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+	dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+}
 
 	@Test
 	void contextLoads() {
