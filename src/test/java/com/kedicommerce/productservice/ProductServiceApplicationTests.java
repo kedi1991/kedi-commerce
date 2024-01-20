645b77d0e2a42aec6b339a051cfc59e2f412f24e
OKURUT Henry Kedi <hokurut@netcompany.com>
2024-01-21 00:30:35 +0100

    comfigure mockmvc for testing using mockservelet to call the controllers

--- src/test/java/com/kedicommerce/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/kedicommerce/productservice/ProductServiceApplicationTests.java
@@ -1,16 +1,20 @@
 package com.kedicommerce.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestConstructor;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 	/**
 	 * the mongoDBContainer no-args constructor is deprecated
@@ -18,13 +22,16 @@ class ProductServiceApplicationTests {
 	@Container
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
 
-@DynamicPropertySource
-static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
-	dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-}
+	@Autowired //used to inject the bean (Instantiated object of a class)
+	private MockMvc mockMvc;
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+
 	}
 
 }
