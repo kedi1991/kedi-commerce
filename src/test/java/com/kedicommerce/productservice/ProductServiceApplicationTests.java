f6ba8ddf0c5c3eca8aaaac095c4993bbdf162fda
OKURUT Henry Kedi <hokurut@netcompany.com>
2024-01-21 01:15:29 +0100

    add product request to the mockMvc.perform method

--- src/test/java/com/kedicommerce/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/kedicommerce/productservice/ProductServiceApplicationTests.java
@@ -1,16 +1,23 @@
 package com.kedicommerce.productservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kedicommerce.productservice.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
 
 @Testcontainers
 @SpringBootTest
@@ -24,14 +31,35 @@ class ProductServiceApplicationTests {
 
 	@Autowired //used to inject the bean (Instantiated object of a class)
 	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
 		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 
 	@Test
-	void shouldCreateProduct() {
+	void shouldCreateProduct() throws Exception {
 
+		ProductRequest productRequest = getProductRequest();
+
+		/**
+		 * content method takes a string, so we have to convert the product request to a string
+		 */
+		String productRequestString  = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString)
+		);
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("kedi")
+				.description("kedi")
+				.price(BigDecimal.valueOf(150))
+				.build();
 	}
 
 }
